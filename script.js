41100d3caf7b42f1786a925021df6ca46d571344
Nezar Babeka <71599268+NXR8@users.noreply.github.com>
2025-03-17 11:11:27 +0200

    fix
    Fix button action

--- script.js
+++ script.js
@@ -23,20 +23,24 @@ function sendTelegramMessage(message) {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-
-    
-    document.querySelector('#try-it-link').addEventListener('click', function(event) {
-        fetch('https://ipapi.co/json')
-            .then(response => response.json())
-            .then(data => {
-                var ip = data.ip;
-                var isp = data.org;
-                var country = data.country_name;
-                var city = data.region;
-                var message = `CV%0AIP Address: ${ip}%0AISP: ${isp}%0ACountry: ${country}%0ACity: ${city}%0AClicked to try NY Encryption`;
-            sendTelegramMessage(message);
+    document.querySelectorAll('#try-it-link').forEach(function(link) {
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+            var buttonName = link.dataset.buttonName;
+            fetch('https://ipapi.co/json')
+                .then(response => response.json())
+                .then(data => {
+                    var ip = data.ip;
+                    var isp = data.org;
+                    var country = data.country_name;
+                    var city = data.region;
+                    var message = `CV%0AIP Address: ${ip}%0AISP: ${isp}%0ACountry: ${country}%0ACity: ${city}%0AClicked: ${buttonName}`;
+                    sendTelegramMessage(message);
+                    window.open(link.href, '_blank');
+                })
+                .catch(error => {
+                    console.error("Error fetching IP data:", error);
+                });
         });
     });
 });
-
-
